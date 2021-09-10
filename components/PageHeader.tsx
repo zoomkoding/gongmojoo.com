e117d9d633386360aad7da0d7d86db2c44a2b084
zoomkoding <zoomkoding@gmail.com>
2021-09-11 02:08:09 +0900

    PageHeader 구현

---
+++ components/PageHeader.tsx
@@ -0,0 +1,29 @@
+import classes from "./PageHeader.module.scss";
+
+function PageHeader() {
+  return (
+    <header className={classes.header}>
+      <div className={classes.headerContent}>
+        <div className={classes.logo}>모두를 위한 공모주</div>
+        <div className={classes.menus}>
+          <div className={classes.menu}>
+            <a>공모주란</a>
+          </div>
+          <div className={classes.menu}>
+            <a>진행중인 공모주</a>
+          </div>
+          <div className={classes.menu}>
+            <a>공모주 캘린더</a>
+          </div>
+          <div className={classes.menu}>
+            <a>계좌 개설 정보</a>
+          </div>
+          <div className={classes.menu}>
+            <a>피드백 남기기</a>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
+export default PageHeader;
