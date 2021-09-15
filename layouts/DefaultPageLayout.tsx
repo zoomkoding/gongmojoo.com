957927fb2daad89228663fb50c7c91d10fadcdea
zoomkoding <zoomkoding@gmail.com>
2021-09-15 11:36:18 +0900

    google analytics 추가

--- layouts/DefaultPageLayout.tsx
+++ layouts/DefaultPageLayout.tsx
@@ -15,6 +15,20 @@ function DefaultPageLayout({ children }: IDefaultPageLayoutProps) {
           name="google-site-verification"
           content="c0gtAYO2TBUvjFC3scycauGVmPPzOmd96LC6RIbKIiQ"
         />
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-G8M1X540VP"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-G8M1X540VP');
+            `,
+          }}
+        />
       </Head>
       <PageHeader />
       <main className={classes.main}>{children}</main>
