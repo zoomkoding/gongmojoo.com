1fa59dbc149e99568d808759fd7e02efd48e2aba
zoomkoding <zoomkoding@gmail.com>
2021-09-18 01:27:27 +0900

    StockSecurity Entity 구현

---
+++ server/src/gongmo/entity/stock-security.entity.ts
@@ -0,0 +1,43 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class StockSecurity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'integer' })
+  securityId: number;
+
+  @Column({ type: 'integer' })
+  stockId: number;
+
+  @Column({ type: 'varchar', length: 45 })
+  증권사이름: string;
+
+  @Column({ type: 'float' })
+  일반경쟁률: number;
+
+  @Column({ type: 'float' })
+  비례경쟁률: number;
+
+  @Column({ type: 'integer' })
+  총청약건수: number;
+
+  @Column({ type: 'integer' })
+  일반균등물량: number;
+
+  @Column({ type: 'integer' })
+  일반비례물량: number;
+
+  @CreateDateColumn()
+  createdAt: string;
+
+  @UpdateDateColumn()
+  updatedAt: string;
+}
