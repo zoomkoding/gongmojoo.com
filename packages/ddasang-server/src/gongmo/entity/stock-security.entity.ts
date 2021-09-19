fccacdd650b2cf583ce8ac2216e324c3be21fb67
zoomkoding <zoomkoding@gmail.com>
2021-09-19 13:21:52 +0900

    공모주주간사 테이블 업데이트

--- packages/ddasang-server/src/gongmo/entity/stock-security.entity.ts
+++ packages/ddasang-server/src/gongmo/entity/stock-security.entity.ts
@@ -17,19 +17,19 @@ export class StockSecurity {
   @Column({ type: 'varchar', length: 45 })
   증권사이름: string;
 
-  @Column({ type: 'float' })
+  @Column({ type: 'float', nullable: true })
   일반경쟁률: number;
 
-  @Column({ type: 'float' })
+  @Column({ type: 'float', nullable: true })
   비례경쟁률: number;
 
-  @Column({ type: 'integer' })
+  @Column({ type: 'integer', nullable: true })
   총청약건수: number;
 
-  @Column({ type: 'integer' })
+  @Column({ type: 'integer', nullable: true })
   일반균등물량: number;
 
-  @Column({ type: 'integer' })
+  @Column({ type: 'integer', nullable: true })
   일반비례물량: number;
 
   @CreateDateColumn()
