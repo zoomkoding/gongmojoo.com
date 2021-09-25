c6a85d950dff1a9549fb1a23d0cb2eb3815e979f
zoomkoding <zoomkoding@gmail.com>
2021-09-25 16:19:52 +0900

    StockListSection에서 서로 간의 간격 추가

--- packages/ddasang-web/components/StockListSection.tsx
+++ packages/ddasang-web/components/StockListSection.tsx
@@ -2,18 +2,21 @@ import { IStock } from "@/../types";
 import React from "react";
 import Section, { ISectionProps } from "./Section";
 import StockCard from "./StockCard";
+import classes from "./StockListSection.module.scss";
 
 export interface IStockListSectionProps extends ISectionProps {
   stocks: IStock[];
 }
 
 function StockListSection({ stocks, ...sectionProps }: IStockListSectionProps) {
-  console.log(stocks);
+  if (!stocks || stocks.length === 0) return null;
   return (
     <Section {...sectionProps}>
-      {stocks.map((stock) => (
-        <StockCard stock={stock} key={stock.id} />
-      ))}
+      <div className={classes.stockCards}>
+        {stocks.map((stock) => (
+          <StockCard stock={stock} key={stock.id} />
+        ))}
+      </div>
     </Section>
   );
 }
