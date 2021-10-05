3cd04d44bd14a9ddbbec1691c9c21a3d1aae8efc
zoomkoding <zoomkoding@gmail.com>
2021-10-05 13:25:54 +0900

    timezone 설정

--- packages/ddasang-web/pages/_app.tsx
+++ packages/ddasang-web/pages/_app.tsx
@@ -1,8 +1,15 @@
+import dayjs from "dayjs";
 import "dayjs/locale/ko";
+import timezone from "dayjs/plugin/timezone";
+import utc from "dayjs/plugin/utc";
 import "framework7-icons/css/framework7-icons.css";
 import type { AppProps } from "next/app";
 import "../styles/fonts.css";
 import "../styles/globals.scss";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.tz.setDefault("Asia/Seoul");
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
